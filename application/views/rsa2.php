0ec77f5fd24a3615cee85bc87e70928d63b5dda5
Kel Novi <novhz0514@gmail.com>
2016-06-01 09:58:49 -0700

    Update rsa2.php

--- application/views/rsa2.php
+++ application/views/rsa2.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> RSA AJAX</title>
+	<title> RSA </title>
 	<script type="text/javascript" src="<?php echo base_url('jquery.js'); ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('jquery.jcryption.3.1.0.js'); ?>"></script>
 </head>
@@ -10,7 +10,7 @@
 <form id="ajaxtest" method="POST" action="<?php echo base_url('rsa/test2_res');?>">
 	<input type="text" name="firstname" id="firstname"/>
 	<input type="password" name="pw" id="pw" />
-	<button type="submit" id="btntest" name="btntest"> Make Request via AJAX</button>
+	<button type="submit" id="btntest" name="btntest"> Submit Form</button>
 </form>
 
 		<script type="text/javascript">
